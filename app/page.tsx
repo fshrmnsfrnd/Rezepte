977cb6bc32940c352646b25acedad1d054fb791c
fshrmnsfrnd <fshrmnsfrnd@web.de>
2025-11-21 01:31:57 +0100

    mainpage sieht geil aus

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,41 @@ import React, { useState } from "react";
 
 export default function Home() {
   const [filteredCocktailIds, setFilteredCocktailIds] = useState<number[] | null>(null);
+  const [selectedPanel, setSelectedPanel] = useState<'cocktails' | 'ingredients'>('cocktails');
 
   return (
     <div>
       <header>
         <a href="/"><h1>Cocktails</h1></a>
+        <div className="showRow">
+          <div
+            role="button"
+            className={`showArea ${selectedPanel === 'ingredients' ? 'active' : ''}`}
+            onClick={() => setSelectedPanel('ingredients')}
+          >
+            <h3>Zutaten</h3>
+          </div>
+          <div
+            role="button"
+            className={`showArea ${selectedPanel === 'cocktails' ? 'active' : ''}`}
+            onClick={() => setSelectedPanel('cocktails')}
+          >
+            <h3>Cocktails</h3>
+          </div>
+        </div>
       </header>
-        <IngredientList onFilterChange={setFilteredCocktailIds} />
-        <AllCocktails filterIds={filteredCocktailIds} />
+
+      <div className="mainRow">
+        
+
+        <div className="displayArea" data-visible={selectedPanel === 'ingredients' ? 'true' : 'false'}>
+          <IngredientList onFilterChange={setFilteredCocktailIds} />
+        </div>
+
+        <div className="displayArea" data-visible={selectedPanel === 'cocktails' ? 'true' : 'false'}>
+          <AllCocktails filterIds={filteredCocktailIds} />
+        </div>
+      </div>
     </div>
   );
 }
