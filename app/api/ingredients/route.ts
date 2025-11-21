6097663979d6bfcbb57067aac9a76fdf20e55f3c
fshrmnsfrnd <fshrmnsfrnd@web.de>
2025-11-21 01:43:57 +0100

    optionale zutaten werden nicht angezeigt

--- app/api/ingredients/route.ts
+++ app/api/ingredients/route.ts
@@ -2,6 +2,15 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
 export async function GET(request: Request) {
-    const ingredients = await db.all("SELECT Ingredient_ID, Name FROM Ingredient");
+    const sql = `
+        SELECT DISTINCT i.Ingredient_ID, i.Name
+        FROM Ingredient i
+        JOIN Cocktail_Ingredient ci
+            ON i.Ingredient_ID = ci.Ingredient_ID
+        WHERE ci.Optional = 0;
+        ORDER BY Name ASC
+    `;
+    
+    const ingredients = await db.all(sql);
     return NextResponse.json(ingredients);
 }
