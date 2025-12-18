c8378ba00641246043a3fb7bc2840a9dc6811d61
fshrmnsfrnd <fshrmnsfrnd@web.de>
2025-12-18 18:28:48 +0100

    css umbenannt

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React, { Suspense, useEffect, useState } from "react";
+
+export default function RecipeDetail() {
+
+    return (
+        <div>
+            <header className="header">
+                <a href="/"><h1 className="h1">Rezepte</h1></a>
+            </header>
+            
+        </div>
+    )
+}
